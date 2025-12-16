d79b2d87726373cd390f958430b34fb3e9753e28
gilmerperez <gilmer2002@outlook.com>
2025-12-15 22:29:14 -0500

    Created 3 help and support cards

--- src/pages/Help/Help.jsx
+++ src/pages/Help/Help.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import styles from "./Help.module.css";
 import { Link } from "react-router-dom";
+import styles from "./Help.module.css";
 
 function Help() {
   // * Set page title
@@ -12,21 +12,38 @@ function Help() {
     <>
       <main>
         <div className={styles.helpContainer}>
-          {/* Orders */}
-          <Link to="help/orders">
-            <h2>Orders</h2>
-            <p>Learn about placing and managing orders</p>
-          </Link>
-          {/* Returns */}
-          <Link to="help/returns">
-            <h2>Returns</h2>
-            <p>Learn about returning and refunding orders</p>
-          </Link>
-          {/* Shipping */}
-          <Link to="help/shipping">
-            <h2>Shipping</h2>
-            <p>Learn about shipping and delivery of orders</p>
-          </Link>
+          {/* Title */}
+          <h1 className={styles.title}>HELP AND SUPPORT</h1>
+
+          {/* Help and support cards */}
+          <div className={styles.cardsContainer}>
+            {/* Orders */}
+            <div className={styles.helpCard}>
+              <h2 className={styles.cardTitle}>Orders</h2>
+              <p className={styles.cardText}>Learn about placing and managing orders</p>
+              <Link to="/help/orders" className={styles.viewAllButton}>
+                VIEW ALL
+              </Link>
+            </div>
+
+            {/* Returns  */}
+            <div className={styles.helpCard}>
+              <h2 className={styles.cardTitle}>Returns</h2>
+              <p className={styles.cardText}>Learn about returning and refunding orders</p>
+              <Link to="/help/returns" className={styles.viewAllButton}>
+                VIEW ALL
+              </Link>
+            </div>
+
+            {/* Shipping */}
+            <div className={styles.helpCard}>
+              <h2 className={styles.cardTitle}>Shipping</h2>
+              <p className={styles.cardText}>Learn about shipping and delivery of orders</p>
+              <Link to="/help/shipping" className={styles.viewAllButton}>
+                VIEW ALL
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
     </>
