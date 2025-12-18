a74b16f8b7c387fca096bc32f329b18e1d88456b
gilmerperez <gilmer2002@outlook.com>
2025-12-18 17:36:05 -0500

    Further styled latin america tour concert content

--- src/pages/Tour/components/Concert/Concert.jsx
+++ src/pages/Tour/components/Concert/Concert.jsx
@@ -2,40 +2,33 @@ import styles from "./Concert.module.css";
 
 function Concert({ date, stadium, location, ticketsLink, vipTicketsLink }) {
   return (
-    <>
-      <div className={styles.concertContainer}>
-        <div className={styles.concertInfo}>
-          {/* Concert date */}
-          <p className={styles.date}>{date}</p>
-          {/* Concert stadium */}
-          <p className={styles.stadium}>{stadium}</p>
-          {/* Concert location */}
-          <p className={styles.location}>{location}</p>
-        </div>
-        <div className={styles.concertLinks}>
-          {/* Tickets link */}
-          <a
-            target="_blank"
-            href={ticketsLink}
-            rel="noopener noreferrer"
-            className={styles.ticketsLink}
-            aria-label={`Buy tickets for ${stadium} on ${date}`}
-          >
-            TICKETS
-          </a>
-          {/* VIP tickets link */}
-          <a
-            target="_blank"
-            href={vipTicketsLink}
-            rel="noopener noreferrer"
-            className={styles.vipTicketsLink}
-            aria-label={`Buy VIP tickets for ${stadium} on ${date}`}
-          >
-            VIP TICKETS
-          </a>
-        </div>
+    <div className={styles.concertContainer}>
+      {/* Concert information */}
+      <p className={styles.date}>{date}</p>
+      <p className={styles.stadium}>{stadium}</p>
+      <p className={styles.location}>{location}</p>
+      {/* Concert tickets */}
+      <div className={styles.concertLinks}>
+        <a
+          target="_blank"
+          href={ticketsLink}
+          rel="noopener noreferrer"
+          className={styles.ticketsLink}
+          aria-label={`Buy tickets for ${stadium} on ${date}`}
+        >
+          TICKETS
+        </a>
+        <a
+          target="_blank"
+          href={vipTicketsLink}
+          rel="noopener noreferrer"
+          className={styles.vipTicketsLink}
+          aria-label={`Buy VIP tickets for ${stadium} on ${date}`}
+        >
+          VIP TICKETS
+        </a>
       </div>
-    </>
+    </div>
   );
 }
 
